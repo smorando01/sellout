a9ca61fdbb240d4393eca6ef94ab1fa60fba0c8b
Santiago <santiago@amvuy.com>
2025-11-29 13:31:54 -0300

    Cambios para poder registrar

--- index.php
+++ index.php
@@ -210,7 +210,7 @@ $hoy = new DateTimeImmutable('today');
     </div>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-3fp9Qx9S3n4n2ZT1JZM6ORtztpEDXjIM5bUoD2yUksk=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
